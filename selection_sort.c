66cc3d0cac5e64b3aeee5032e60f9926bcbf41a8
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2024-07-17 17:24:26 +0530

    selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+int main()
+{
+    int arr[100];
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    selectionSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
